a7ab3ea4d009bbcade51577aafedac5539877658
cobramaster <omarcorporated@gmail.com>
2017-10-06 02:14:52 -0400

    mistake

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 import tweepy, time, random
 from SPylib import *
 
-consumer_key = 'hg4362Sz3sgkXIenj5WMvOO04'
-consumer_secret = 'V8lEbVid61uBJy3W0UX2nNp7z7lAXPLYOPcQIlYZQJCNPrcajp'
-access_token = '900453085876768770-74jZd2G14bTRVEeF8U8Khk5UGyTUsXz'
-access_token_secret = '3b3wnz1OU7f95Io5T4EXyYv3g4PuHQst0J9M2GE5JkTOd'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
