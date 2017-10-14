82868d6627012feeed2fde5d5a259ca33c5afd04
cobramaster <omarcorporated@gmail.com>
2017-10-14 17:06:08 -0400

    Update 2

--- main.py
+++ main.py
@@ -3,14 +3,12 @@
 import tweepy
 from SPylib import *
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
-unfollowAll(api)
